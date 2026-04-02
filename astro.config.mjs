1bffd483a50a6a3a73decec47e448f00a4ec6c61
Ahmad Izzuddin Azzam <diegomortar@gmail.com>
2026-04-02 11:35:50 +0700

    Fix : Deployment issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,17 @@
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
+import vercel from "@astrojs/vercel"
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [react()],
   vite: /** @type {any} */ ({
     plugins: [tailwindcss()],
     server: {
       allowedHosts: ["screwed-nonhedonistically-joycelyn.ngrok-free.dev"],
     },
   }),
-  integrations: [react()],
 })
